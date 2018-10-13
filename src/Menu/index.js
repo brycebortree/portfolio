6bd534b996400c1255a66d35be7e51e261e1de94
Bryce Bortree <bryce.bortree@outreach.io>
2018-10-13 14:33:57 -0700

    Add placeholder menu (#2)
    * Add simple sticky placeholder menu
    
    * Shift assets

---
+++ src/Menu/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+        <p>I am a Menu!</p>
+      </div>
+    );
+  }
+}
+
+export default Menu;
